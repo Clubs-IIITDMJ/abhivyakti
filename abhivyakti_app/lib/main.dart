b70160bf1fa56ba3cac4b7fbe30b010fcd7f4e12
Rishabh_Negi <nrishabh20@gmail.com>
2021-05-20 18:52:01 +0530

    redirect link

--- abhivyakti_app/lib/main.dart
+++ abhivyakti_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,88 +30,99 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ResponsiveBuilder(
-      builder: (context, sizeInfo) {
-        return Column(
-          children: [
-            Expanded(
-              flex: 4,
-              child: Center(
-                child: Stack(
-                  children: [
-                    Positioned(
-                      bottom: -32,
-                      left: 28,
-                      right: -28,
-                      child: Container(
-                        margin: EdgeInsets.symmetric(horizontal: 40),
-                        child: SvgPicture.asset(
-                          'assets/svg/abhivyakti-text-1.svg',
-                          fit: BoxFit.contain,
-                          color: Color(0xFFE5E5E5),
+    return Scaffold(
+      body: ResponsiveBuilder(
+        builder: (context, sizeInfo) {
+          return Column(
+            children: [
+              Expanded(
+                flex: 4,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: -32,
+                        left: 28,
+                        right: -28,
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 40),
+                          child: SvgPicture.asset(
+                            'assets/svg/abhivyakti-text-1.svg',
+                            fit: BoxFit.contain,
+                            color: Color(0xFFE5E5E5),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 40),
-                      child: SvgPicture.asset(
-                        'assets/svg/abhivyakti-text-2.svg',
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    Positioned(
-                      top: -32,
-                      left: -28,
-                      right: 28,
-                      child: Container(
+                      Container(
                         margin: EdgeInsets.symmetric(horizontal: 40),
                         child: SvgPicture.asset(
-                          'assets/svg/abhivyakti-text-3.svg',
+                          'assets/svg/abhivyakti-text-2.svg',
                           fit: BoxFit.contain,
                         ),
                       ),
+                      Positioned(
+                        top: -32,
+                        left: -28,
+                        right: 28,
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 40),
+                          child: SvgPicture.asset(
+                            'assets/svg/abhivyakti-text-3.svg',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Text(
+                    'Website currently under construction',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Lufga',
+                      fontSize:
+                          sizeInfo.deviceScreenType == DeviceScreenType.mobile
+                              ? 20
+                              : 30,
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Text(
-                  'Website currently under construction',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Lufga',
-                    fontSize:
-                        sizeInfo.deviceScreenType == DeviceScreenType.mobile
-                            ? 20
-                            : 30,
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                  'INSTAGRAM',
-                  style: TextStyle(
-                    color: Color(0xFFFF2C2C),
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'Lufga',
-                    fontSize:
-                        sizeInfo.deviceScreenType == DeviceScreenType.mobile
-                            ? 20
-                            : 30,
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () async {
+                      if (await canLaunch(
+                          'https://www.instagram.com/abhivyakti_iiitdmj/'))
+                        await launch(
+                            'https://www.instagram.com/abhivyakti_iiitdmj/');
+                    },
+                    child: Text(
+                      'INSTAGRAM',
+                      style: TextStyle(
+                        color: Color(0xFFFF2C2C),
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Lufga',
+                        fontSize:
+                            sizeInfo.deviceScreenType == DeviceScreenType.mobile
+                                ? 20
+                                : 30,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
-        );
-      },
-    ));
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
 }
